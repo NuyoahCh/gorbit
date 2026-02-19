f9f656285545c4e1aa643525ac83aa5feebd4538
wangchen <springoflotus@gmail.com>
2026-02-19 21:16:40 +0800

    feat: add example of goroutines and their usage in hello.go

---
+++ example/Golang并发编程/03-并发组件/01-goroutine/hello.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func excute() {
+	// 启动一个新的 goroutine 来执行 sayHello 函数
+	go sayHello()
+	// 主 goroutine 继续执行其他代码
+	fmt.Println("hello world")
+}
+
+func sayGoodbye() {
+	go func() {
+		fmt.Println("Goodbye, Goroutine!")
+	}()
+	// 主 goroutine 继续执行其他代码
+	fmt.Println("goodbye world")
+
+	// 直接在 goroutine 中定义和调用匿名函数
+	sayGo := func() {
+		fmt.Println("Go, Goroutine!")
+	}
+	go sayGo()
+}
+
+func sayHello() {
+	fmt.Println("Hello, Goroutine!")
+}
