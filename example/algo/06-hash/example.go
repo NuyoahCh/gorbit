bf59fe9914687d868247213297fc2fb12641896e
wangchen <springoflotus@gmail.com>
2026-02-24 19:13:26 +0800

    refactor: Simplify MyHashMap methods by removing key parameter from hash function

--- example/algo/06-hash/example.go
+++ example/algo/06-hash/example.go
@@ -13,24 +13,24 @@ func Constructor() MyHashMap {
 
 // 增/改，复杂度 O(1)
 func (h *MyHashMap) Put(key, value interface{}) {
-	index := h.hash(key)
+	index := h.hash()
 	h.table[index] = value
 }
 
 // 查，复杂度 O(1)
 func (h *MyHashMap) Get(key interface{}) interface{} {
-	index := h.hash(key)
+	index := h.hash()
 	return h.table[index]
 }
 
 // 删，复杂度 O(1)
 func (h *MyHashMap) Remove(key interface{}) {
-	index := h.hash(key)
+	index := h.hash()
 	h.table[index] = nil
 }
 
 // 哈希函数，把 key 转化成 table 中的合法索引
 // 时间复杂度必须是 O(1)，才能保证上述方法的复杂度都是 O(1)
-func (h *MyHashMap) hash(key interface{}) int {
+func (h *MyHashMap) hash() int {
 	return 0
 }
